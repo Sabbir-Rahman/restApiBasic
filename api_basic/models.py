45fd37ffd200430b153856eabf1ddf30b2b8e7ff
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-02-14 13:35:43 +0600

    serializer basic

--- api_basic/models.py
+++ api_basic/models.py
@@ -7,3 +7,7 @@ class Article(models.Model):
     author = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     date = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
