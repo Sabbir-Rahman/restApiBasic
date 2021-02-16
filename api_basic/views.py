948737500e6f3158c55181a5156d45b9bc24c3b6
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-02-16 11:11:24 +0600

    post and get request of serializer work

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Article
+from .serializers import ArticleSerializers
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@csrf_exempt
+def article_list(request):
+
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializers(articles, many= True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        info = JSONParser().parse(request)
+        serializer = ArticleSerializers(data=info)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    
+
+
